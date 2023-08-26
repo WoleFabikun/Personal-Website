862e2dafc11225ff37f93d0c024803c5d92aef70
Wole Fabikun <wolefabikun@Woles-MacBook-Air.local>
2023-08-26 09:20:54 -0400

    Created working branch

--- pages/index.js
+++ pages/index.js
@@ -56,13 +56,13 @@ const Home = ({ posts, projects, skill, mdxSource, frontmatter: { title } }) => 
 
 				{/* About Me */}
 				<section className="flex flex-col gap-10 mt-8 mb-8 text-lg leading-8 text-left text-slate-200">
-					<div className="flex flex-col items-center border rounded-lg shadow-md dark:border-slate-900 lg:flex-row bg-[#262640] dark:bg-transparent">
+					<div className="flex flex-col items-center border rounded-lg shadow-md dark:border-slate-900 lg:flex-row bg-[#eeeef3] dark:bg-transparent">
 						<Image className="object-cover w-full rounded-t-lg h-96 lg:h-120 xl:h-100 xl:w-200 md:rounded-none md:rounded-l-lg" src={Headshot2} alt="" />
 						<div className="flex flex-col justify-between p-4 leading-normal">
 							<h1 className="mb-2 text-2xl font-bold tracking-tight ">{title}</h1>
 							<MDXRemote {...mdxSource} components={{
-								p: ({ children, ...props }) => <p className="mb-3 text-base font-normal text-slate-100" {...props}>{children}</p>,
-								a: ({ children, ...props }) => <a className="text-blue-300 hover:underline underline-offset-4 " {...props}>{children}</a>,
+								p: ({ children, ...props }) => <p className="mb-3 text-base font-normal dark:text-slate-200 text-blue-600" {...props}>{children}</p>,
+								a: ({ children, ...props }) => <a className="dark:text-blue-300 text-blue-900 hover:underline underline-offset-4 " {...props}>{children}</a>,
 							}} />
 						</div>
 					</div>
