5cde01067f0354929423e4362cf76b0540052a27
Wole Fabikun <wole359@gmail.com>
2024-03-14 16:16:31 -0400

    update about

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "96f6c5e570e231e5"; }
+/******/ 		__webpack_require__.h = function() { return "b259628d98624751"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.0e98c6138de28fd1.hot-update.js
+++ .next/static/webpack/webpack.0e98c6138de28fd1.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "059c85e0c626f50d"; }
+/******/ 	__webpack_require__.h = function() { return "b259628d98624751"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
