d4f4c81c29b438c929bd2e9a892257bed4cee5f0
Wole Fabikun <wole359@gmail.com>
2024-04-19 22:59:00 -0400

    quick fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "121f33012baa6b0a"; }
+/******/ 		__webpack_require__.h = function() { return "d9964b63fcf70cd6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
