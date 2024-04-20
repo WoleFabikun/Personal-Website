49c03052ecaee3d9a7c883e81032537f3311ea6b
Wole Fabikun <wole359@gmail.com>
2024-04-19 22:05:44 -0400

    new project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b259628d98624751"; }
+/******/ 		__webpack_require__.h = function() { return "121f33012baa6b0a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -12,7 +12,7 @@ export default function ProjectCard(props) {
     const stackList = stack.includes(' — ') ? stack.split(' — ') : stack.split('—');
 
     return (
-        <Link href={url} rel="noopener noreferrer" className="content-end h-full duration-300 ease-in-out delay-150 border border-gray-700 rounded-lg shadow-md bg-[#eeeef3] dark:bg-transparent dark:drop-shadow-lg dark:shadow-gray-900/100 border-zinc-100/10 dark:hover:border-zinc-200/50 dark:hover:inner-shadow hover:transition hover:-translate-y-2 hover:scale-105">
+        <Link href={url} rel="noopener noreferrer" className="flex content-end h-full duration-300 ease-in-out delay-150 border border-gray-700 rounded-lg shadow-md bg-[#eeeef3] dark:bg-transparent dark:drop-shadow-lg dark:shadow-gray-900/100 border-zinc-100/10 dark:hover:border-zinc-200/50 dark:hover:inner-shadow hover:transition hover:-translate-y-2 hover:scale-105">
             <div className="p-5">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight dark:text-slate-200 text-blue-600">{title}</h5>
                 <span className="items-end block mt-2 text-lg font-semibold dark:text-slate-200 text-blue-600">
